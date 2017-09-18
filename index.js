0d0a49566d78eb640f47a34669d70deffc401875
Ian Copp <copp.ian@gmail.com>
2017-09-18 12:47:59 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const { getLoader } = require("react-app-rewired");
+
+function rewireNearley(config, env, nearleyLoaderOptions = {}) {
+  const nearleyExtension = /\.ne$/;
+
+  const fileLoader = getLoader(
+    config.module.rules,
+    rule =>
+      rule.loader &&
+      typeof rule.loader === "string" &&
+      rule.loader.endsWith(`file-loader${path.sep}index.js`)
+  );
+  fileLoader.exclude.push(nearleyExtension);
+
+  const nearleyRules = {
+    test: nearleyExtension,
+    loader: "nearley-loader"
+  };
+
+  config.module.rules.push(nearleyRules);
+
+  return config;
+}
+
+module.exports = rewireNearley;
