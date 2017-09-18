5aa6326b3a31b7c3a86e45374099fe2507d5c0d5
Ian Copp <copp.ian@gmail.com>
2017-09-18 13:01:33 -0700

    Update prettier config

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const path = require("path");
-const { getLoader } = require("react-app-rewired");
+const path = require('path')
+const { getLoader } = require('react-app-rewired')
 
 function rewireNearley(config, env, nearleyLoaderOptions = {}) {
-  const nearleyExtension = /\.ne$/;
+  const nearleyExtension = /\.ne$/
 
   const fileLoader = getLoader(
     config.module.rules,
     rule =>
       rule.loader &&
-      typeof rule.loader === "string" &&
+      typeof rule.loader === 'string' &&
       rule.loader.endsWith(`file-loader${path.sep}index.js`)
-  );
-  fileLoader.exclude.push(nearleyExtension);
+  )
+  fileLoader.exclude.push(nearleyExtension)
 
   const nearleyRules = {
     test: nearleyExtension,
-    loader: "nearley-loader"
-  };
+    loader: 'nearley-loader'
+  }
 
-  config.module.rules.push(nearleyRules);
+  config.module.rules.push(nearleyRules)
 
-  return config;
+  return config
 }
 
-module.exports = rewireNearley;
+module.exports = rewireNearley
