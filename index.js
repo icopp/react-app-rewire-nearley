646f42bac78a3301f7a15b277420bed3af009181
Ian Copp <copp.ian@gmail.com>
2017-11-01 18:25:17 -0700

    Correct for issue caused by react-scripts update

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-const path = require('path')
-const { getLoader } = require('react-app-rewired')
+/**
+ * @param {Object} rule
+ * @return {Array}
+ */
+const ruleChildren = rule =>
+  rule.use || rule.oneOf || (Array.isArray(rule.loader) && rule.loader) || []
+
+const findIndexAndRules = (rulesSource, ruleMatcher) => {
+  let result
+  const rules = Array.isArray(rulesSource)
+    ? rulesSource
+    : ruleChildren(rulesSource)
+  rules.some(
+    (rule, index) =>
+      (result = ruleMatcher(rule)
+        ? { index, rules }
+        : findIndexAndRules(ruleChildren(rule), ruleMatcher))
+  )
+  return result
+}
+
+/**
+* Given a rule, return if it uses a specific loader.
+*/
+const createLoaderMatcher = loader => rule =>
+  rule.loader && rule.loader.indexOf(`/${loader}/`) !== -1
+
+/**
+* Get the existing file-loader config.
+*/
+const fileLoaderMatcher = createLoaderMatcher('file-loader')
+
+/**
+* Add one rule before another in the list of rules.
+*/
+const addBeforeRule = (rulesSource, ruleMatcher, value) => {
+  const { index, rules } = findIndexAndRules(rulesSource, ruleMatcher)
+  rules.splice(index, 0, value)
+}
 
 function rewireNearley(config, env, nearleyLoaderOptions = {}) {
   const nearleyExtension = /\.ne$/
-
-  const fileLoader = getLoader(
-    config.module.rules,
-    rule =>
-      rule.loader &&
-      typeof rule.loader === 'string' &&
-      rule.loader.endsWith(`file-loader${path.sep}index.js`)
-  )
-  fileLoader.exclude.push(nearleyExtension)
 
   const nearleyRules = {
     test: nearleyExtension,
@@ -19,7 +47,8 @@ function rewireNearley(config, env, nearleyLoaderOptions = {}) {
     options: nearleyLoaderOptions
   }
 
-  config.module.rules.push(nearleyRules)
+  // Add the Nearley rule before the file-loader rule.
+  addBeforeRule(config.module.rules, fileLoaderMatcher, nearleyRules)
 
   return config
 }
